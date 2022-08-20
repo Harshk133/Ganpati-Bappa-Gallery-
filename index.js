3362030bb06c96ea8775c9b5615b2f7b44528847
Harsh <harshmkale.2004@gmail.com>
2022-08-20 21:33:08 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hello world!");
+
+// Constructor
+function productName(name, type, price){
+    this.name = name;
+    this.type = type;
+    this.price = price;
+}
+
+// Display Constructor
+function Display(){
+
+}
+
+// Add methods to display prototypes
